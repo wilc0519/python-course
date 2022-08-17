ea9daf2ebc719bee65663878614f2e3e32cb1c28
wilc0519 <wilc0519@gmail.com>
2022-08-17 18:52:58 -0500

    functions

--- sintaxisBasica/primeras_funciones.py
+++ sintaxisBasica/primeras_funciones.py
@@ -1,5 +1,9 @@
-def mensaje():
-    print("incursionando en pyton")
-    print("primeras funciones en python")
+import math
+def ecuacion_segundo_grado(a, b, c):
+    primera_raiz = (-b + math.sqrt(math.pow(b, 2)- 4*a*c))/2*a
+    segunda_raiz = (-b - math.sqrt(math.pow(b, 2)- 4*a*c))/2*a
 
-mensaje()
+    return primera_raiz ,segunda_raiz
+
+
+print(ecuacion_segundo_grado(1, 3, 2))
