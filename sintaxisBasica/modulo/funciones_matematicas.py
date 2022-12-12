9853b68abcfd4ef40d832f7e70b4ebd5ae165a50
wilc0519 <wilc0519@gmail.com>
2022-12-12 09:06:10 -0500

    string methods and modules

---
+++ sintaxisBasica/modulo/funciones_matematicas.py
@@ -0,0 +1,8 @@
+def add(num1, num2):
+    print("the result of addition is: ", num1 + num2)
+
+def subtract(num1, num2):
+    print("the result of subtraction is: ", num1 - num2)
+
+def multiply(num1, num2):
+    print("the result of multiplication is: ", num1 * num2)
